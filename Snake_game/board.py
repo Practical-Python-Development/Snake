d87da41d28c34fd7764dc60214bd81bb5a45c69e
jnnkhffr <jannik.hoeffer@gmx.de>
2025-07-13 12:09:30 +0200

    The game board is finished and runs without errors. Now work on the main and see if the board also runs in main.

---
+++ Snake_game/board.py
@@ -0,0 +1,22 @@
+import pygame
+
+class Board:
+    def __init__(self, cols=20, rows=20, block_size=20,
+                 bgcolor=(0, 0, 0), gridcolor=(40, 40, 40)):
+        self.cols = cols
+        self.rows = rows
+        self.block_size = block_size
+        self.bgcolor = bgcolor
+        self.gridcolor = gridcolor
+
+    def draw(self, surface):
+        surface.fill(self.bgcolor)
+        for x in range(self.cols):
+            for y in range(self.rows):
+                rect = pygame.Rect(
+                    x * self.block_size,
+                    y * self.block_size,
+                    self.block_size,
+                    self.block_size
+                )
+                pygame.draw.rect(surface, self.gridcolor, rect, 1)
