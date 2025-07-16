176470f6f49505d64236f9e4fffde9022df16044
jnnkhffr <jannik.hoeffer@gmx.de>
2025-07-16 13:05:55 +0200

    First improvements but the test still doesnt run.

--- Snake_game/unit_test.py
+++ Snake_game/unit_test.py
@@ -1,19 +1,27 @@
 """ This skript tests the main Snake Programm"""
 
 import unittest
+import main
+from board import Board, Food
 
-COLS, ROWS = 30, 30
+#COLS, ROWS = 30, 30
 
 class TestSnakeMovement(unittest.TestCase):
     def test_move_20_steps_right(self):
         """
-        Simuliere 20 Moves nach rechts und stelle sicher,
+        Idee: Simuliere 20 Moves nach rechts und stelle sicher,
         dass die Schlange sich korrekt verschiebt und
         immer innerhalb der Grenzen bleibt.
         """
         # Start snake
         snake = [(0, 0), (-1, 0)]
-        #direction = (1, 0)
+        direction = (1, 0)
+
+        for step in range(20):
+            head_x, head_y = snake[0]
+            new_head = (head_x + direction[0], head_y + direction[1])
+
+            self.assertTrue(new_head)
 
 
         self.assertEqual(snake[0], (20, 0))
